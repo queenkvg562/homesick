8cb76741872ecc3885a6c62cf9ad24a53deec956
queenkvg562 <qubhgcref@outlook.com>
2024-08-13 18:34:29 +0800

    Create homesick.java

---
+++ homesick.java
@@ -0,0 +1,54 @@
+import java.io.*;
+ 
+public class EmployeeTest {
+    public static void main(String[] args) {
+        // 使用构造器创建两个对象
+        Employee empOne = new Employee("RUNOOB1");
+        Employee empTwo = new Employee("RUNOOB2");
+ 
+        // 调用这两个对象的成员方法
+        empOne.setAge(26);
+        empOne.setDesignation("高级程序员");
+        empOne.setSalary(1000);
+        empOne.printEmployee();
+ 
+        empTwo.setAge(21);
+        empTwo.setDesignation("菜鸟程序员");
+        empTwo.setSalary(500);
+        empTwo.printEmployee();
+    }
+}
+public class Penguin { 
+    private String name; 
+    private int id; 
+    public Penguin(String myName, int  myid) { 
+        name = myName; 
+        id = myid; 
+    } 
+    public void eat(){ 
+        System.out.println(name+"正在吃"); 
+    }
+    public void sleep(){
+        System.out.println(name+"正在睡");
+    }
+    public void introduction() { 
+        System.out.println("大家好！我是"         + id + "号" + name + "."); 
+    } 
+}
+public class Mouse { 
+    private String name; 
+    private int id; 
+    public Mouse(String myName, int  myid) { 
+        name = myName; 
+        id = myid; 
+    } 
+    public void eat(){ 
+        System.out.println(name+"正在吃"); 
+    }
+    public void sleep(){
+        System.out.println(name+"正在睡");
+    }
+    public void introduction() { 
+        System.out.println("大家好！我是"         + id + "号" + name + "."); 
+    } 
+}
